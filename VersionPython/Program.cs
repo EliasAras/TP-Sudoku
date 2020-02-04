c1645ad64d8761a2a4625a76b84e3403412883c6
Thomas Dudu <thomas.duvergne@orange.fr>
2020-02-04 13:18:24 +0100

    Cs X Python

---
+++ VersionPython/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace RunPythonScriptFromCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execute python process...");
+            Option1_ExecProcess();
+
+            Console.WriteLine();
+
+
+
+            Console.ReadKey();
+        }
+
+        static void Option1_ExecProcess()
+        {
+            // 1) Create Process Info
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Users\thoma\AppData\Local\Programs\Python\Python38-32\python.exe";
+
+            // 2) Provide script and arguments
+            var script = @"C:\Users\thoma\Desktop\Cours ING4\IA\TP-Sudoku-master\Sudoku-Solver-CNN-master\model.py";
+            var game = "080032001703080002500007030050001970600709008047200050020600009800090305300820010";
+
+            psi.Arguments = $"\"{script}\" \"{game}\"";
+
+            // 3) Process configuration
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            // 4) Execute process and get output
+            var errors = "";
+            var results = "";
+
+            using (var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+                results = process.StandardOutput.ReadToEnd();
+            }
+
+            // 5) Display output
+            Console.WriteLine("ERRORS:");
+            Console.WriteLine(errors);
+            Console.WriteLine();
+            Console.WriteLine("Results:");
+            Console.WriteLine(results);
+
+        }
+
+
+    }
+}
+
